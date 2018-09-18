21d2f7e76a897ccb6d61aa1d68513274741e41d5
ginpei <ginpei@gmail.com>
2018-09-18 11:07:41 -0700

    extract

--- src/pages/paint/PaintMenu.tsx
+++ src/pages/paint/PaintMenu.tsx
@@ -124,11 +124,21 @@ class PaintMenu extends React.Component<IPaintMenuProps, IPaintMenuState> {
   }
 
   public render () {
+    const body = this.renderBody();
+
     return (
       <div className={`PaintMenu ${this.props.visible ? '-visible' : ''}`}
         onClick={this.onClick}
         >
         <AppHeader fullscreen={true}/>
+        {body}
+      </div>
+    );
+  }
+
+  protected renderBody () {
+    return (
+      <div className="PaintMenu-body">
         <div className="PaintMenu-close">
           <i className="fa fa-times" aria-hidden="true" />
         </div>
