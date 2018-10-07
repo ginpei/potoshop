1065df1f3470721bbc2721ed54fb37b6c8e4bd89
ginpei <ginpei@gmail.com>
2018-10-07 00:36:33 -0700

    edit from history

--- src/pages/CreateNewPage.tsx
+++ src/pages/CreateNewPage.tsx
@@ -1,27 +1,42 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import AppFooter from '../components/AppFooter';
 import AppHeader from '../components/AppHeader';
 import { appHistory, appSpace } from '../misc';
+import firebase from '../plugins/firebase';
+import * as image from '../services/image';
 import './CreateNewPage.css';
 
 type ICreateNewPagePros = any;
 interface ICreateNewPageState {
   height: number;
+  imageRecords: image.IImageRecord[];
+  loadingImages: boolean;
   width: number;
 }
 
 class CreateNewPage extends React.Component<ICreateNewPagePros, ICreateNewPageState> {
+  protected currentUser: firebase.User | null;
+  protected unsubscribes: firebase.Unsubscribe[] = [];
+
+  protected get uid () {
+    return this.currentUser ? this.currentUser.uid : '';
+  }
+
   constructor (props: ICreateNewPagePros) {
     super(props);
     this.state = {
       height: 0,
+      imageRecords: [],
+      loadingImages: true,
       width: 0,
     };
-    this.onChange = this.onChange.bind(this);
   }
 
   public render () {
+    const onChange = this.onChange.bind(this);
     const onSubmitNew = this.onSubmitNew.bind(this);
+    const records = this.state.imageRecords;
 
     return (
       <div className="CreateNewPage">
@@ -33,16 +48,40 @@ class CreateNewPage extends React.Component<ICreateNewPagePros, ICreateNewPageSt
             <input className="CreateNewPage-sizeInput" type="number"
               name="width"
               value={this.state.width}
-              onChange={this.onChange}
+              onChange={onChange}
               />
             x
             <input className="CreateNewPage-sizeInput" type="number"
               name="height"
               value={this.state.height}
-              onChange={this.onChange}
+              onChange={onChange}
               />
             <button>Create new</button>
           </form>
+        </div>
+        <div className="container">
+          <h1>Edit recent item</h1>
+          <p>Note: this editing creates a new image, duplicating the selected image.</p>
+          <div className="CreateNewPage-recordList">
+            {this.state.loadingImages ? 'Loading...' :
+              records.length < 1 ? 'No images' :
+              records.map((record) => (
+                <Link
+                  className="CreateNewPage-recordItem"
+                  key={record.id}
+                  to={`/paint?newType=history&uid=${this.uid}&id=${record.id}`}
+                  >
+                  <img
+                    className="CreateNewPage-image"
+                    alt=""
+                    src={record.url}
+                    width={record.width}
+                    height={record.height}
+                    />
+                </Link>
+              ))
+            }
+          </div>
         </div>
         <AppFooter/>
       </div>
@@ -55,6 +94,13 @@ class CreateNewPage extends React.Component<ICreateNewPagePros, ICreateNewPageSt
       height: el.clientHeight - appSpace * 2,
       width: el.clientWidth - appSpace * 2,
     });
+
+    const onAuthStateChanged = this.onAuthStateChanged.bind(this);
+    this.unsubscribes.push(firebase.auth().onAuthStateChanged(onAuthStateChanged));
+  }
+
+  public componentWillUnmount () {
+    this.unsubscribes.forEach(v => v());
   }
 
   protected onSubmitNew (event: React.FormEvent<HTMLFormElement>) {
@@ -79,6 +125,25 @@ class CreateNewPage extends React.Component<ICreateNewPagePros, ICreateNewPageSt
     } else {
       throw new Error(`Unknown input named ${name}`);
     }
+  }
+
+  protected async onAuthStateChanged (user: firebase.User) {
+    this.currentUser = user;
+    this.setState({
+      imageRecords: await this.fetchList(),
+    });
+  }
+
+  protected async fetchList (): Promise<image.IImageRecord[]> {
+    if (this.currentUser) {
+      this.setState({ loadingImages: true });
+      const images = await image.fetchList(this.currentUser.uid);
+      this.setState({ loadingImages: false });
+      return images;
+    }
+
+    this.setState({ loadingImages: false });
+    return [];
   }
 }
 
