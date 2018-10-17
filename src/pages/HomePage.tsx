f6fc299301e6caaaeaa689a92f6abb3756eaa19c
ginpei <ginpei@gmail.com>
2018-10-16 21:06:39 -0700

    install as PWA (hope works)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,25 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import NiceButton from 'src/components/NiceButton';
 import AppFooter from '../components/AppFooter';
 import AppHeader from '../components/AppHeader';
 import './HomePage.css';
 
 type IHomePagePros = any;
-type IHomePageState = any;
+interface IHomePageState {
+  beforeInstallPromptEvent: Event | null;
+}
 
 class HomePage extends React.Component<IHomePagePros, IHomePageState> {
+  constructor (props: IHomePagePros) {
+    super(props);
+    this.state = {
+      beforeInstallPromptEvent: null,
+    };
+    this.onBeforeInstallPrompt = this.onBeforeInstallPrompt.bind(this);
+    this.onInstallClick = this.onInstallClick.bind(this);
+  }
+
   public render () {
     return (
       <div className="HomePage">
@@ -32,11 +44,47 @@ class HomePage extends React.Component<IHomePagePros, IHomePageState> {
               <i className="fa fa-info-circle" aria-hidden="true"/>
               About
             </Link>
+            {this.state.beforeInstallPromptEvent && <NiceButton
+              icon="cloud-download"
+              onClick={this.onInstallClick}
+              >
+              {' '}
+              Install
+            </NiceButton>}
           </div>
         </div>
         <AppFooter/>
       </div>
     );
+  }
+
+  public componentWillMount () {
+    window.addEventListener('beforeinstallprompt', this.onBeforeInstallPrompt);
+  }
+
+  public componentWillUnmount () {
+    window.removeEventListener('beforeinstallprompt', this.onBeforeInstallPrompt);
+  }
+
+  protected onBeforeInstallPrompt (event: Event) {
+    this.setState({
+      beforeInstallPromptEvent: event,
+    });
+  }
+
+  protected onInstallClick () {
+    this.install();
+  }
+
+  protected async install () {
+    const event = this.state.beforeInstallPromptEvent as any;
+    event.prompt();
+    const result = await event.userChoice;
+    if (result.outcome === 'accepted') {
+      this.setState({
+        beforeInstallPromptEvent: null,
+      });
+    }
   }
 }
 
